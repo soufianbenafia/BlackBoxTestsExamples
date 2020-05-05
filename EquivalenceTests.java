5e162279bb1b52565476ea324b1b276a1fbcf850
Soufian Ben Afia <soufian.benafia@gmx.de>
2020-05-05 16:35:03 +0200

    dataprovider added

--- EquivalenceTests.java
+++ EquivalenceTests.java
@@ -1,134 +1,45 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 class BlackBoxTest_Template {
 
-	
+	@ParameterizedTest
+	public static Collection<Object[]> data() {
+		return Arrays.asList(new Object[][] { { 15, 8, Color.RED }, { 18, 9, Color.BLUE }, { 18, 9, Color.BLUE },
+				{ 9, 8, Color.RED }, { 11, -2, Color.YELLOW }, { 19, -5, Color.YELLOW }, { 9, -1, Color.YELLOW },
+				{ 13, 5, Color.YELLOW }, { 20, 5, Color.YELLOW }, { 5, 1, Color.YELLOW }, { 17, 8, Color.YELLOW },
+				{ 17, -2, Color.YELLOW }, { 17, 5, Color.YELLOW },
+
+		});
+	}
+
+	private final Color expected;
+	private final int x;
+	private final int y;
+
+	public BlackBoxTest_Template(int x, int y,Color expected) {
+		this.expected = expected;
+		this.x = x;
+		this.y = y;
+	}
+
 	@Test
-	void testCaseOne() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.RED;
-		int x = 15;
-		int y = 8;
+	void test() {
 		Color actual = BlackBox.G(x, y);
 		assertEquals(expected, actual);
 	}
-	
-	@Test
-	void testCaseTwo() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.BLUE;
-		int x = 18;
-		int y = 9;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	void testCaseThree() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.RED;
-		int x = 9;
-		int y = 8;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	void testCaseFour() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.YELLOW;
-		int x = 11;
-		int y = -2;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	void testCaseFive() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.YELLOW;
-		int x = 19;
-		int y = -5;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	void testCaseSix() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.GREEN;
-		int x = 9;
-		int y = -1;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	//YELLOW
-	
-	@Test
-	void testCaseSeven() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.YELLOW;
-		int x = 13;
-		int y = 5;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	void testCaseEight() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.YELLOW;
-		int x = 20;
-		int y = 5;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	
-	@Test
-	void testCaseNine() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.YELLOW;
-		int x = 5;
-		int y = 1;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	void testCaseTen() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.YELLOW;
-		int x = 17;
-		int y = 8;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	void testCaseEleven() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.YELLOW;
-		int x = 17;
-		int y = -2;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
-	@Test
-	void testCaseTwelve() {
-		/* Colors: YELLOW, GREEN, RED */ 
-		Color expected = Color.YELLOW;
-		int x = 17;
-		int y = 5;
-		Color actual = BlackBox.G(x, y);
-		assertEquals(expected, actual);
-	}
-	
+
 	// ------- DO NOT TOUCH BELOW THIS LINE -------
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
