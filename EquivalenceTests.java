0f5245f023ede17aed23b7b743b86776a665b558
Soufian Ben Afia <soufian.benafia@gmx.de>
2020-05-05 16:45:29 +0200

    Dataprovider junit5 version

--- EquivalenceTests.java
+++ EquivalenceTests.java
@@ -5,37 +5,22 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
+import org.junit.jupiter.params.provider.MethodSource;
 
-@RunWith(Parameterized.class)
 class BlackBoxTest_Template {
 
-	@ParameterizedTest
-	public static Collection<Object[]> data() {
+	public static Collection<Object[]> equivalenceClassesDataProvider() {
 		return Arrays.asList(new Object[][] { { 15, 8, Color.RED }, { 18, 9, Color.BLUE }, { 18, 9, Color.BLUE },
-				{ 9, 8, Color.RED }, { 11, -2, Color.YELLOW }, { 19, -5, Color.YELLOW }, { 9, -1, Color.YELLOW },
+				{ 9, 8, Color.RED }, { 11, -2, Color.YELLOW }, { 19, -5, Color.YELLOW }, { 9, -1, Color.GREEN },
 				{ 13, 5, Color.YELLOW }, { 20, 5, Color.YELLOW }, { 5, 1, Color.YELLOW }, { 17, 8, Color.YELLOW },
 				{ 17, -2, Color.YELLOW }, { 17, 5, Color.YELLOW },
-
 		});
 	}
 
-	private final Color expected;
-	private final int x;
-	private final int y;
-
-	public BlackBoxTest_Template(int x, int y,Color expected) {
-		this.expected = expected;
-		this.x = x;
-		this.y = y;
-	}
-
-	@Test
-	void test() {
+	@ParameterizedTest
+	@MethodSource("equivalenceClassesDataProvider")
+	void testEquivalenceClasses(int x, int y, Color expected) {
 		Color actual = BlackBox.G(x, y);
 		assertEquals(expected, actual);
 	}
